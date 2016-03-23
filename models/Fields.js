88ae21ef914d922ab61e1deb0b6d73c8aca1dd8d
sdurbin <sdurbin@keywcorp.com>
2016-03-23 16:17:30 +0000

    Added a datatype field to Fields and modified database data to contain datatype

--- models/Fields.js
+++ models/Fields.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var FieldSchema = new mongoose.Schema({
   name: String,
+  datatype: String,
   source: { type: mongoose.Schema.Types.ObjectId, ref: 'Source' }
 });
 
