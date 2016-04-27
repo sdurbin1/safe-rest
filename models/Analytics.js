73dff9b19bbbc2630681e7d4c3e8f2c0b4c02b3d
sdurbin <sdurbin@keywcorp.com>
2016-04-27 18:57:40 +0000

    Removed analyticParams from analytics

--- models/Analytics.js
+++ models/Analytics.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var AnalyticSchema = new mongoose.Schema({
   name: String,
-  analyticParams: mongoose.Schema.Types.Mixed,
   visualizationTypes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'VisualizationType' }]
 });
 
