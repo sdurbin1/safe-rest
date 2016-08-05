d1185e76ef2133f1eaf491d0b5e140238f61a8e7
sdurbin <sdurbin@keywcorp.com>
2016-08-05 18:26:15 +0000

    Linted. Also, closes #103

--- utils/mongoExecute.js
+++ utils/mongoExecute.js
@@ -22,7 +22,7 @@ function mongoExecute (requestBody, db, visualization) {
         resolve(average(queryJson, db, visualization, limit))
       } else if (visualization.analytic.name === 'Detailed Count') {
         resolve(detailedCount(queryJson, db, visualization, limit))
-      } else if (visualization.visualizationType.name === 'Summary') { 
+      } else if (visualization.visualizationType.name === 'Summary') {
         resolve(summaryCount(queryJson, db, visualization, limit))
       } else {
         resolve(search(queryJson, db, visualization, limit))
@@ -143,8 +143,6 @@ function average (queryJson, db, visualization, limit) {
 function summaryCount (queryJson, db, visualization, limit) {
   return new Promise(function (resolve, reject) {
     const src = visualization.source._id
-    const summaryValues = visualization.analyticParams.summaryValues
-    const collection = db.collection('' + src)
     
     return mongoUtil.queryMongo(db, src.toString(), queryJson, limit)
       .then(function (out) {
@@ -156,24 +154,6 @@ function summaryCount (queryJson, db, visualization, limit) {
 }
 
 function transformBasicAverage (raw) {
-  const output = []
-  
-  for (let i = 0; i < raw.length; i = i + 1) {
-    const record = {}
-    
-    for (const k in raw[i]['_id']) {
-      record['Value'] = raw[i]['_id'][k]
-    }
-        
-    record['Average'] = raw[i].average
-    output.push(record)
-  }
-  
-  return output
-}
-
-// TODO: Fix this!
-function transformSummaryCount (raw) {
   const output = []
   
   for (let i = 0; i < raw.length; i = i + 1) {
