691cdb8516f8ea9bf069fff3d06ee61c6e2c9b65
sdurbin <sdurbin@keywcorp.com>
2016-09-19 20:41:32 +0000

    Updated db scripts to include db and host option. Closes #135

--- scripts/db/dropDb.sh
+++ scripts/db/dropDb.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo -e "\nUsage: ./scripts/db/dropDb.sh dbName\n"
-  exit
-fi
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-DBNAME=$1
+# Initialize our own variables:
+host="localhost"
+db="safe"
 
-mongo $DBNAME --eval "db.dropDatabase()"
+while getopts "uh:d:" opt; do
+    case "$opt" in
+    u)  
+        echo -e "\nUsage: ./scripts/db/dropDb.sh -h HOST -d DB. Defaults are host: localhost, database: safe.\n"
+        exit 0
+        ;;
+    h)  host=$OPTARG
+        ;;
+    d)  db=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+echo "Mongo options: host='$host', db='$db'"
+
+mongo $db --host $host --eval "db.dropDatabase()"
--- scripts/db/importDb.sh
+++ scripts/db/importDb.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo -e "\nUsage: ./scripts/db/importDb.sh dbName\n"
-  exit
-fi
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-DBNAME=$1
+# Initialize our own variables:
+host="localhost"
+db="safe"
 
-mongoimport --db $DBNAME --collection 56eaec7a2308db1b1c6f795f --file data/safe/56eaec7a2308db1b1c6f795f.json
-mongoimport --db $DBNAME --collection 572a16e5fbdcac91218b9a1c --file data/safe/572a16e5fbdcac91218b9a1c.json
-mongoimport --db $DBNAME --collection 5748845f8058984023ca43d6 --file data/safe/5748845f8058984023ca43d6.json
-mongoimport --db $DBNAME --collection 57a4d2c21c5e543405b806cd --file data/safe/57a4d2c21c5e543405b806cd.json
-mongoimport --db $DBNAME --collection analytics --file data/safe/analytics.json
-mongoimport --db $DBNAME --collection dashboardgroups --file data/safe/dashboardgroups.json
-mongoimport --db $DBNAME --collection dashboards --file data/safe/dashboards.json
-mongoimport --db $DBNAME --collection sources --file data/safe/sources.json
-mongoimport --db $DBNAME --collection visualizations --file data/safe/visualizations.json
-mongoimport --db $DBNAME --collection visualizationtypes --file data/safe/visualizationtypes.json
+while getopts "uh:d:" opt; do
+    case "$opt" in
+    u)  
+        echo -e "\nUsage: ./scripts/db/importDb.sh -h HOST -d DB. Defaults are host: localhost, database: safe.\n"
+        exit 0
+        ;;
+    h)  host=$OPTARG
+        ;;
+    d)  db=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+echo "Mongo options: host='$host', db='$db'"
+
+mongoimport --db $db --host $host --collection 56eaec7a2308db1b1c6f795f --file data/safe/56eaec7a2308db1b1c6f795f.json
+mongoimport --db $db --host $host --collection 572a16e5fbdcac91218b9a1c --file data/safe/572a16e5fbdcac91218b9a1c.json
+mongoimport --db $db --host $host --collection 5748845f8058984023ca43d6 --file data/safe/5748845f8058984023ca43d6.json
+mongoimport --db $db --host $host --collection 57a4d2c21c5e543405b806cd --file data/safe/57a4d2c21c5e543405b806cd.json
+mongoimport --db $db --host $host --collection analytics --file data/safe/analytics.json
+mongoimport --db $db --host $host --collection dashboardgroups --file data/safe/dashboardgroups.json
+mongoimport --db $db --host $host --collection dashboards --file data/safe/dashboards.json
+mongoimport --db $db --host $host --collection sources --file data/safe/sources.json
+mongoimport --db $db --host $host --collection visualizations --file data/safe/visualizations.json
+mongoimport --db $db --host $host --collection visualizationtypes --file data/safe/visualizationtypes.json
